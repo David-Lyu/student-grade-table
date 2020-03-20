76c3783d6575ca47a9e0fcf537f8db346f6e6d19
David Lyu <lyu.david@yahoo.com>
2020-03-20 15:09:03 -0700

    added variable to app js constructor, then added a new file page-header and made a new class PageHeader, created a grid system for header element and added new script, main js added new dom query for header and instantiated new Class

--- components/app.js
+++ components/app.js
@@ -1,9 +1,10 @@
 class App {
 
-  constructor(gradeTable) {
+  constructor(gradeTable,pageHeader) {
     this.bindHandleGetGradesError = this.handleGetGradesError.bind(this);
     this.bindHandleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
     this.gradeTable = gradeTable;
+    this.pageHeader = pageHeader;
   }
 
   handleGetGradesError(error) {
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var tableElement = document.getElementById("table");
+var headerAverages = document.getElementById("updateAverages")
 var gradeTable = new GradeTable(tableElement);
-
-var viewGrades = new App(gradeTable);
+var pageHeader = new PageHeader(headerAverages)
+var viewGrades = new App(gradeTable,pageHeader);
