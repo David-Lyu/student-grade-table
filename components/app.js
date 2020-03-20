b06e9339a3426a8c22fe4ea12108e6b9b457d0e8
David Lyu <lyu.david@yahoo.com>
2020-03-20 13:50:08 -0700

    made a grade table js in components and added a new class and instantiated it, app js takes in new parameter from grade-table, index has another script tag for grade-table, lastly main js instatiates gradetable class and puts in instiated grade table into institated app class

--- components/app.js
+++ components/app.js
@@ -1,26 +1,35 @@
 class App {
 
-  handleGetGradesError(error){
+  constructor(gradeTable) {
+    this.bindHandleGetGradesError = this.handleGetGradesError.bind(this);
+    this.bindHandleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+    this.gradeTable = gradeTable;
+  }
+
+  handleGetGradesError(error) {
     console.error(error);
   }
 
-  handleGetGradesSuccess(grades){
-    console.log(grades);
+  handleGetGradesSuccess(grades) {
+    this.gradeTable.updateGrades(grades)
   }
 
-  constructor(){
-    this.bindHandleGetGradesError = this.handleGetGradesError.bind(this);
-    this.bindHandleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
-  }
-
-  getGrades(){
+  getGrades() {
     jQuery.ajax(
       {
+        headers: {
+          "X-Access-Token": "PPW7pOdc"
+        },
         method: "GET",
-        URL: "https://github.com/Learning-Fuze/sgt_api#get-all-grades",
-        Success: this.bindHandleGetGradesSuccess,
-        Error: this.bindHandleGetGradesError
+        url: "https://sgt.lfzprototypes.com/api/grades",
+        data: "none",
+        success: this.bindHandleGetGradesSuccess,
+        error: this.bindHandleGetGradesError
       }
     )
+  }
+
+  start() {
+    this.getGrades();
   }
 }
