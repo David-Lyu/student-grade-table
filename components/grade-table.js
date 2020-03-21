ebc6faafd226f76068f160313520aafab371ba24
David Lyu <lyu.david@yahoo.com>
2020-03-20 21:22:05 -0700

    Nothing changed put in then took out console.log

--- components/grade-table.js
+++ components/grade-table.js
@@ -4,16 +4,32 @@ class GradeTable {
   }
   updateGrades(grades) {
     var tbodyElement = this.tableElement.querySelector("tbody");
-    for(var gradesIndex = 0; gradesIndex <grades.length; gradesIndex++) {
-      var thElement = document.createElement("tr");
-      var td1Element = document.createElement("td");
-      td1Element.textContent = grades[gradesIndex].name;
+    var tbodyChildren = tbodyElement.querySelectorAll("tr");
+    console.log(grades)
+    if(tbodyChildren.length === 0){
+      for(var gradesIndex = 0; gradesIndex <grades.length; gradesIndex++) {
+        var thElement = document.createElement("tr");
+        var td1Element = document.createElement("td");
+        td1Element.textContent = grades[gradesIndex].name;
+        thElement.appendChild(td1Element);
+        var td2Element = document.createElement("td");
+        td2Element.textContent = grades[gradesIndex].course;
+        thElement.appendChild(td2Element);
+        var td3Element = document.createElement("td");
+        td3Element.textContent = grades[gradesIndex].grade;
+        thElement.appendChild(td3Element);
+        tbodyElement.appendChild(thElement);
+      }
+    }else {
+      thElement = document.createElement("tr");
+      td1Element = document.createElement("td");
+      td1Element.textContent = grades[tbodyChildren.length].name;
       thElement.appendChild(td1Element);
-      var td2Element = document.createElement("td");
-      td2Element.textContent = grades[gradesIndex].course;
+      td2Element = document.createElement("td");
+      td2Element.textContent = grades[tbodyChildren.length].course;
       thElement.appendChild(td2Element);
-      var td3Element = document.createElement("td");
-      td3Element.textContent = grades[gradesIndex].grade;
+      td3Element = document.createElement("td");
+      td3Element.textContent = grades[tbodyChildren.length].grade;
       thElement.appendChild(td3Element);
       tbodyElement.appendChild(thElement);
     }
