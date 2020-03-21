f9977e31d1f44440c29c61a49e86714a63d99612
DavidLyu <59672932+David-Lyu@users.noreply.github.com>
2020-03-20 21:35:00 -0700

    Revert "Add grade"

--- main.js
+++
@@ -1,7 +0,0 @@
-var tableElement = document.getElementById("table");
-var headerAverages = document.getElementById("updateAverages")
-var formElement = document.querySelector("form")
-var gradeForm = new GradeForm(formElement);
-var gradeTable = new GradeTable(tableElement);
-var pageHeader = new PageHeader(headerAverages)
-var viewGrades = new App(gradeTable,pageHeader,gradeForm);
