959bd35784097dec62417d1b7a88f74c8514e872
David Lyu <lyu.david@yahoo.com>
2020-03-20 17:38:44 -0700

    put gradeForm in as a parameter to view grades

--- main.js
+++ main.js
@@ -4,4 +4,4 @@ var formElement = document.querySelector("form")
 var gradeForm = new GradeForm(formElement);
 var gradeTable = new GradeTable(tableElement);
 var pageHeader = new PageHeader(headerAverages)
-var viewGrades = new App(gradeTable,pageHeader);
+var viewGrades = new App(gradeTable,pageHeader,gradeForm);
