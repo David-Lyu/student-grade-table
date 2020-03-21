ffe56c7d08eb116a01d87e623ee911b31437f31e
David Lyu <lyu.david@yahoo.com>
2020-03-21 15:40:57 -0700

    stuck on making new tr and td

--- components/grade-table.js
+++ components/grade-table.js
@@ -1,36 +1,46 @@
 class GradeTable {
-  constructor(tableElement) {
+  constructor(tableElement,noGradesElement) {
     this.tableElement = tableElement;
+    this.noGradesElement = noGradesElement;
   }
-  updateGrades(grades) {
+  updateGrades(grades,renderGradeRow) {
     var tbodyElement = this.tableElement.querySelector("tbody");
-    var tbodyChildren = tbodyElement.querySelectorAll("tr");
-    if(tbodyChildren.length === 0){
-      for(var gradesIndex = 0; gradesIndex <grades.length; gradesIndex++) {
-        var thElement = document.createElement("tr");
-        var td1Element = document.createElement("td");
+    tbodyElement.innerHTML = "";
+    var pElement = document.querySelector("p");
+      if(grades.length != 0) {
+        pElement.classList.add("d-none")
+        for (var gradesIndex = 0; gradesIndex < grades.length; gradesIndex++) {
+        this.renderGradeRow();
         td1Element.textContent = grades[gradesIndex].name;
-        thElement.appendChild(td1Element);
-        var td2Element = document.createElement("td");
         td2Element.textContent = grades[gradesIndex].course;
-        thElement.appendChild(td2Element);
-        var td3Element = document.createElement("td");
         td3Element.textContent = grades[gradesIndex].grade;
-        thElement.appendChild(td3Element);
-        tbodyElement.appendChild(thElement);
+        tbodyElement.appendChild(trElement)
+        this.deleteGrade;
+        }
+      }else {
+        pElement.classList.remove("d-none")
       }
-    }else {
-      thElement = document.createElement("tr");
-      td1Element = document.createElement("td");
-      td1Element.textContent = grades[tbodyChildren.length].name;
-      thElement.appendChild(td1Element);
-      td2Element = document.createElement("td");
-      td2Element.textContent = grades[tbodyChildren.length].course;
-      thElement.appendChild(td2Element);
-      td3Element = document.createElement("td");
-      td3Element.textContent = grades[tbodyChildren.length].grade;
-      thElement.appendChild(td3Element);
-      tbodyElement.appendChild(thElement);
-    }
+  }
+  onDeleteClick(deleteGrade) {
+    this.deleteGrade = deleteGrade;
+  }
+  renderGradeRow(data,deleteGrade){
+    this.single = data;
+    function deleteGrade(){console.log("hello")}
+      var trElement = document.createElement("tr");
+      var td1Element = document.createElement("td");
+      trElement.appendChild(td1Element);
+      var td2Element = document.createElement("td");
+      trElement.appendChild(td2Element);
+      var td3Element = document.createElement("td");
+      trElement.appendChild(td3Element);
+      var td4Element = document.createElement("tr");
+      var tdDeleteButton = document.createElement("button");
+      tdDeleteButton.classList.add("btn,btn-danger");
+      tdDeleteButton.textContent = "DELETE";
+      td4Element.appendChild(tdDeleteButton);
+      trElement.appendChild(td4Element);
+      tdDeleteButton.addEventListener("click", deleteGrade(this.single))
+      return {trElement,td1Element,td2Element,td3Element};
   }
 }
--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var tableElement = document.getElementById("table");
 var headerAverages = document.getElementById("updateAverages")
 var formElement = document.querySelector("form")
+var pElement = tableElement.querySelector("p");
 var gradeForm = new GradeForm(formElement);
-var gradeTable = new GradeTable(tableElement);
+var gradeTable = new GradeTable(tableElement,pElement);
 var pageHeader = new PageHeader(headerAverages)
 var viewGrades = new App(gradeTable,pageHeader,gradeForm);
