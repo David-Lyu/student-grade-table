266c1b6f45837c6d9e7cdb5725d6e3d5707e83df
David Lyu <lyu.david@yahoo.com>
2020-03-21 15:50:09 -0700

    redid the updategrades on gradtable js

--- components/grade-table.js
+++ components/grade-table.js
@@ -5,7 +5,7 @@ class GradeTable {
   updateGrades(grades) {
     var tbodyElement = this.tableElement.querySelector("tbody");
     var tbodyChildren = tbodyElement.querySelectorAll("tr");
-    if(tbodyChildren.length === 0){
+    tbodyElement.innerHTML = ""
       for(var gradesIndex = 0; gradesIndex <grades.length; gradesIndex++) {
         var thElement = document.createElement("tr");
         var td1Element = document.createElement("td");
@@ -19,18 +19,4 @@ class GradeTable {
         thElement.appendChild(td3Element);
         tbodyElement.appendChild(thElement);
       }
-    }else {
-      thElement = document.createElement("tr");
-      td1Element = document.createElement("td");
-      td1Element.textContent = grades[tbodyChildren.length].name;
-      thElement.appendChild(td1Element);
-      td2Element = document.createElement("td");
-      td2Element.textContent = grades[tbodyChildren.length].course;
-      thElement.appendChild(td2Element);
-      td3Element = document.createElement("td");
-      td3Element.textContent = grades[tbodyChildren.length].grade;
-      thElement.appendChild(td3Element);
-      tbodyElement.appendChild(thElement);
-    }
-  }
 }
